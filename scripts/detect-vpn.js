bcf8cc9ac943a7679a962dc40a0a363ae90c5e68
Evan <106435366+Evan0234@users.noreply.github.com>
2024-09-17 15:54:31 -0400

    Update detect-vpn.js

--- scripts/detect-vpn.js
+++ scripts/detect-vpn.js
@@ -1,20 +1,24 @@
+const IPINFO_URL = 'https://ipinfo.io/json?token=899f12fa5133df';
+
 async function detectVPNorProxy() {
     try {
         console.log('Fetching IP info...');
         const response = await fetch(IPINFO_URL);
         console.log('Response received:', response);
+        
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
+        
         const data = await response.json();
         console.log('IP info response:', data); // Debugging line
-
+        
         if (data.privacy) {
             const isVPN = data.privacy.vpn || false;
             const isProxy = data.privacy.proxy || false;
             console.log('Is VPN:', isVPN);
             console.log('Is Proxy:', isProxy);
-
+            
             if (isVPN || isProxy) {
                 console.log('Redirecting to 1ud.website');
                 window.location.href = 'https://1ud.website';
@@ -29,3 +33,6 @@ async function detectVPNorProxy() {
         warningDiv.style.display = 'block';
     }
 }
+
+// Call the function to detect VPN or Proxy
+detectVPNorProxy();
